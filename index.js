80a9fed15ebf593c014e145704efba796ebed80f
klhalvarez <klhalvarez@gmail.com>
2023-01-21 17:34:14 -0500

    Can now successfully start server and add user to 'users' table

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const server = require('./Foundations-Project/server');
+//Creating server:
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = 3000;
+
+const server = express();
+
+server.use(bodyParser.json());
+
+//test endpoint
+server.get('/employee', (req, res) =>{
+    res.status(200).send("You have reached the employee login page.")
+});
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+module.exports = server;
+
+//Simple server working!
+
 const AWS = require('aws-sdk');
 const uuid = require ('uuid');
 
@@ -24,8 +45,6 @@ documentClient.put(params, (err) => {
         console.log("Successfully added user");
     }
 });
-
-//TEST
 
 // function retrieveUserName(username) {
 //     const params = {
